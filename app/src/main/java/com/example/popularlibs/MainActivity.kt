80f388b13394d605b61629a4eb72bf257fc970e6
andrey <a.serbin@litebox.ru>
2021-12-08 18:48:27 +0400

    реализовал функционал "3х счетчиков" без mvp

--- app/src/main/java/com/example/popularlibs/MainActivity.kt
+++ app/src/main/java/com/example/popularlibs/MainActivity.kt
@@ -2,10 +2,52 @@ package com.example.popularlibs
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.popularlibs.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val counters = mutableListOf(0, 0, 0)
+    private val COUNTERS_KEY = "COUNTERS_KEY"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnOne.setOnClickListener {
+            binding.btnOne.text = (++counters[0]).toString()
+        }
+
+        binding.btnTwo.setOnClickListener {
+            binding.btnTwo.text = (++counters[1]).toString()
+        }
+
+        binding.btnThree.setOnClickListener {
+            binding.btnThree.text = (++counters[2]).toString()
+        }
+
+        init()
+    }
+
+    fun init(){
+        binding.btnOne.text = counters[0].toString()
+        binding.btnTwo.text = counters[1].toString()
+        binding.btnThree.text = counters[2].toString()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putIntArray(COUNTERS_KEY, counters.toIntArray())
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        val countersArray = savedInstanceState.getIntArray(COUNTERS_KEY)
+        countersArray?.toList()?.let {
+            counters.clear()
+            counters.addAll(it)
+            init()
+        }
     }
 }
